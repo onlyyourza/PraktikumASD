4079752e062519173b36578f16e336854209ceff
onlyyourza <paseoking456@gmail.com>
2026-02-12 11:30:12 +0700

    create new file looping

---
+++ Jobsheet1/Looping.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Looping {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.print("Masukkan n: ");
+        n = input.nextInt();
+        if (n < 10) {
+            n+= 10;
+        }
+
+        for ( int i = 1; i <= n; i++) {
+            if (i == 10 || i == 15) {
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.print("#");
+            } else if (i % 2 == 0) {
+                System.out.print(i);
+            } else {
+                System.out.print("*");
+            }
+        }
+        input.close();
+    }
+}
