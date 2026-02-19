bb18e8863028034b3ebda9d5c1537a9b31b03141
onlyyourza <paseoking456@gmail.com>
2026-02-19 10:16:01 +0700

    2.	Re-open the Mahasiswa Class. Create another object named mhs2 using a parameterized constructor.

--- Jobsheet2/StudentMain14.java
+++ Jobsheet2/StudentMain14.java
@@ -2,16 +2,20 @@ package Jobsheet2;
 
 public class StudentMain14 {
     public static void main(String[] args) {
-        Mahasiswa14 mhsl = new Mahasiswa14();
+        Mahasiswa14 mhs1 = new Mahasiswa14();
         
-        mhsl.nama = "Mafaza";
-        mhsl.nim = "254107060130";
-        mhsl.kelas = "TI-1A";
-        mhsl.ipk = 3.55;
+        mhs1.nama = "Mafaza";
+        mhs1.nim = "254107060130";
+        mhs1.kelas = "TI-1A";
+        mhs1.ipk = 3.55;
 
-        mhsl.tampilkanInformasi();
-        mhsl.ubahKelas("SI 2K");
-        mhsl.updateIpk(3.60);
-        mhsl.tampilkanInformasi();
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa14 mhs2 = new Mahasiswa14("Faza", "254107060131", 3.20, "TI-1B");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
