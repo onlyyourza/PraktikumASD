d71e117820e5d38b063233db56b268d3b9f7baaf
onlyyourza <paseoking456@gmail.com>
2026-04-10 15:12:04 +0700

    modified B. SORTING – SELECTION SORT

--- Jobsheet6/Sorting14.java
+++ Jobsheet6/Sorting14.java
@@ -21,6 +21,20 @@ public class Sorting14 {
         }
     }
 
+    public void selectionSort() {
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (data[j] < data[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = data[minIndex];
+            data[minIndex] = data[i];
+            data[i] = temp;
+        }
+    }
+
     public void print() {
         for (int num : data) {
             System.out.print(num + " ");
--- Jobsheet6/SortingMain14.java
+++ Jobsheet6/SortingMain14.java
@@ -3,15 +3,27 @@ package Jobsheet6;
 public class SortingMain14 {
     public static void main(String[] args) {
         int[] a = {34, 7, 23, 32, 5, 62};
-
-        Sorting14 sorting = new Sorting14(a);
+        Sorting14 sorting1 = new Sorting14(a);
 
         System.out.println("Original array:");
-        sorting.print();
+        sorting1.print();
         
-        sorting.bubbleSort();
+        sorting1.bubbleSort();
         
         System.out.println("Sorted array (Bubble Sort):");
-        sorting.print();
+        sorting1.print();
+
+        System.out.println("\n---------------------------\n");
+
+        int[] b = {30, 20, 2, 8, 14};
+        Sorting14 sorting2 = new Sorting14(b);
+
+        System.out.println("Original array:");
+        sorting2.print();
+        
+        sorting2.selectionSort();
+        
+        System.out.println("Sorted array (Selection Sort):");
+        sorting2.print();
     }
 }
